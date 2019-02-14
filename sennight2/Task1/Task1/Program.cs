85d649acb2a1716fbdc50412d1ea802a1be90a45
dronmage90 <dronmage90@gmail.com>
2019-02-14 19:57:27 +0600

    0001

---
+++ sennight2/Task1/Task1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+namespace Task1
+{
+    class Program
+    {
+
+        public static bool palindrome(string s)
+        {
+            char[] letter = new char[s.Length];
+            letter = s.ToCharArray();
+            int length = s.Length;
+            for (int i = 0; i < length / 2; i++)
+            {
+                if (letter[i] != letter[length - i - 1])
+                    return false;
+            }
+            return true;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Title = "Palindrome";
+            Console.Clear();
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Black;
+            string direction = (@"C:\Users\Андрей\Desktop\PP2\Week2\input1.txt");
+            string check = File.ReadAllText(direction);
+            if (palindrome(check) == true)
+            {
+                Console.Clear();
+                Console.BackgroundColor = ConsoleColor.White;
+                Console.ForegroundColor = ConsoleColor.Black;
+                Console.WriteLine("Yes");
+            }
+            else if (palindrome(check) == false)
+            {
+                Console.Clear();
+                Console.BackgroundColor = ConsoleColor.White;
+                Console.ForegroundColor = ConsoleColor.Black;
+                Console.WriteLine("No");
+            }
+            //Console.Clear();
+            //Console.BackgroundColor = ConsoleColor.White;
+            //Console.ForegroundColor = ConsoleColor.Black;
+            Console.ReadKey();
+        }
+    }
+}
---
+++ sennight2/Task2/Task2/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Task2
+{
+    class Program
+    {
+        public static bool isprime(int a)
+        {
+            int k = 0;
+            for (int i = 2; i < a; i++)
+            {
+                if (a % i == 0)
+                {
+                    k++;
+                }
+            }
+            if (k == 0) return true;
+            else return false;
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.Title = "Prime";
+            string s = File.ReadAllText(@"C:\Users\Андрей\Desktop\PP2\Week2\input2.txt");
+            int[] a = new int[s.Length];
+            string[] str = new string[s.Length];
+            str = s.Split();
+            for (int i = 0; i < str.Length; i++)
+            {
+                a[i] = int.Parse(str[i]);
+            }
+            StreamWriter sw = new StreamWriter(@"C:\Users\Андрей\Desktop\PP2\Week2\output2.txt");
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (isprime(a[i]) == true)
+                {
+                    Console.Clear();
+                    Console.BackgroundColor = ConsoleColor.White;
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    sw.Write(a[i]);
+                    sw.Write(' ');
+                }
+            }
+            sw.Close();
+            Console.ReadKey();
+
+        }
+    }
+}
